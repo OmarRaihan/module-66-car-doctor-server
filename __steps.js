c82dee430704bc271767a536582d911014a7fb0b
OmarRaihan <omarraihan91@gmail.com>
2022-04-26 23:07:01 +0600

    steps added

--- __steps.js
+++ __steps.js
@@ -12,4 +12,6 @@
  * 1. heroku create
  * 2. make sure: git add . git commit git push
  * 3. git push heroku main
+ * 4.Go to Heroku Dashboard > Current Project > setting > Reveal Config
+ * 5. copy paste config vars from .env file
  */
